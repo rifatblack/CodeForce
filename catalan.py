9bbec58e244df6442fef556300d1f80cc2797532
S.M. Rifat <55460433+rifatblack@users.noreply.github.com>
2023-02-14 18:58:45 +0600

    1st commit

---
+++ catalan.py
@@ -0,0 +1,12 @@
+# A recursive function to find nth catalan number
+def catalan(n):
+    # Base Case
+    if n <= 1:
+        return 1
+ 
+    # Catalan(n) is the sum of catalan(i)*catalan(n-i-1)
+    res = 0
+    for i in range(n):
+        res += catalan(i) * catalan(n-i-1)
+ 
+    return res
